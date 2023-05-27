34bab6f3a10c77a3197e2625706436b17e238b6e
betina410 <betina410@seznam.cz>
2023-05-27 15:07:49 +0200

    inital commit

---
+++ Kalkulacka.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulackaApp
+{
+	/// <summary>
+	/// Reprezentuje jednoduchou kalkulačku
+	/// </summary>
+	public class Kalkulacka
+	{
+
+		/// <summary>
+		/// Sečte 2 čísla
+		/// </summary>
+		/// <param name="a">První číslo</param>
+		/// <param name="b">Druhé číslo</param>
+		/// <returns>Součet 2 čísel</returns>
+		public double Secti(double a, double b)
+		{
+			return a + b;
+		}
+
+		/// <summary>
+		/// Odečte 2 čísla
+		/// </summary>
+		/// <param name="a">První číslo</param>
+		/// <param name="b">Druhé číslo</param>
+		/// <returns>Rozdíl 2 čísel</returns>
+		public double Odecti(double a, double b)
+		{
+			return a - b;
+		}
+
+		/// <summary>
+		/// Vynásobí 2 čísla
+		/// </summary>
+		/// <param name="a">První číslo</param>
+		/// <param name="b">Druhé číslo</param>
+		/// <returns>Součin 2 čísel</returns>
+		public double Vynasob(double a, double b)
+		{
+			return a * b;
+		}
+
+		/// <summary>
+		/// Vydělí 2 čísla
+		/// </summary>
+		/// <param name="a">První číslo</param>
+		/// <param name="b">Druhé číslo</param>
+		/// <returns>Podíl 2 čísel</returns>
+		public double Vydel(double a, double b)
+		{
+			if (b == 0)
+				throw new ArgumentException("Nulou nelze dělit!");
+			return a / b;
+			//return 1;
+		}
+
+	}
+}
+
